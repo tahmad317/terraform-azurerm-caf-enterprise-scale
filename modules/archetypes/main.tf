0730fea0e3a8ef71aec70aafb654cca24652d8a8
tahmad317 <98864175+tahmad317@users.noreply.github.com>
2023-03-12 15:43:54 -0400

    Update main.tf

--- modules/archetypes/main.tf
+++ modules/archetypes/main.tf
@@ -1,2 +1,58 @@
 # No resources deployed by this module so this file is here as an entry point only
 # Please navigate the variables, locals and outputs to see how the data model is generated from the inputs
+# Get the current client configuration from the AzureRM provider.
+# This is used to populate the root_parent_id variable with the
+# current Tenant ID used as the ID for the "Tenant Root Group"
+# Management Group.
+
+data "azurerm_client_config" "core" {}
+
+# Declare the Azure landing zones Terraform module
+# and provide a base configuration.
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "<version>" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = var.root_id
+  root_name      = var.root_name
+  library_path   = "${path.root}/lib"
+
+  custom_landing_zones = {
+    "${var.root_id}-online-example-1" = {
+      display_name               = "${upper(var.root_id)} Online Example 1"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id   = "customer_online"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+    "${var.root_id}-online-example-2" = {
+      display_name               = "${upper(var.root_id)} Online Example 2"
+      parent_management_group_id = "${var.root_id}-landing-zones"
+      subscription_ids           = []
+      archetype_config = {
+        archetype_id = "customer_online"
+        parameters = {
+          Deny-Resource-Locations = {
+            listOfAllowedLocations = ["eastus", ]
+          }
+          Deny-RSG-Locations = {
+            listOfAllowedLocations = ["eastus", ]
+          }
+        }
+        access_control = {}
+      }
+    }
+  }
+
+}
